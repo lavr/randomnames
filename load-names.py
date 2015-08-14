92b65308fafe9d1ea3fdc0965bdb91e263c050b0
Sergey Lavrinenko <s@lavr.me>
2015-08-15 00:18:39 +0300

    ...

--- load-names.py
+++ load-names.py
@@ -13,9 +13,14 @@ GROUPS = (
 def get_label(item_id):
     if isinstance(item_id, int):
         item_id = "Q{}".format(item_id)
-    url = "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids={}".format(item_id)
+    url = "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&languages=en&props=labels&ids={}".format(item_id)
     data = requests.get(url).json()['entities'][item_id]
-    return data['labels'].get('en', {}).get('value')
+    return data.get('labels',{}).get('en', {}).get('value')
+
+import re
+NON_ASCII = re.compile(r"[^A-Za-z0-9\- ]")
+def filter_name(name):
+    return not NON_ASCII.search(name)
 
 
 def main():
@@ -24,7 +29,7 @@ def main():
         for item in items:
             label = get_label(item)
             if label:
-                print("{}:{}:{}".format(title, item, label.lower()))
+                print("{}:{}:{}:{}".format(title, item, 'simple' if filter_name(label) else 'complex', label))
 
 if __name__ == "__main__":
     main()
